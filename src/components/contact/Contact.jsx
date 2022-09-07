afcda163aa56b04c576c9922f4689b20716f9760
BakareHameed <bakarehameed07@gmail.com>
2022-09-07 19:48:32 +0100

    created the contact and portfolio components

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 import '../contact/contact.css'
+import {MdOutlineMail} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
+
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in touch</h5>
+      <h2>Contact me</h2>
+      
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <MdOutlineMail/>
+            <h4>Email</h4>
+            <h5>bakarehameed07@gmail.com</h5>
+            <a href="mailto:bakarehameed07@gmail.com">Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp/>
+            <h4>Whatsapp</h4>
+            <h5>+2349060180937</h5>
+            <a href="https://api.whatsapp.com/send?phone+2349060180937" target='_blank' rel="noreferrer">Send a message</a>
+          </article>
+        </div>
+
+        <form action="">
+          <input type="text" name='name' placeholder='Your full name' required/>
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea name="message" placeholder='Your mesasage' id="" cols="30" rows="10"></textarea>
+          <button type='submit' className='btn btn-primary'>Send message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import './portfolio.css'
+import huddle from '../../assets/desktop-preview.jpg'
+import space from '../../assets/mars.jpg'
+import basket from '../../assets/active-states-basket-empty.jpg'
+import intro from '../../assets/intro-section.jpg'
+import nft from '../../assets/nft.jpg'
+import todo from '../../assets/active-states-dark.jpg'
 
 const Portfolio = () => {
   return (
@@ -8,7 +14,66 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        
+        <article className='portfolio__item'>
+          <div className="portfolio_item-image">
+            <img src={huddle} alt="huddle site" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='cta'>
+            <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio_item-image">
+            <img src={space} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='cta'>
+            <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio_item-image">
+            <img src={basket} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='cta'>
+            <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio_item-image">
+            <img src={intro} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='cta'>
+            <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio_item-image">
+            <img src={nft} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='cta'>
+            <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio_item-image">
+            <img src={todo} alt=""/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='cta'>
+            <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          </div>
+        </article>
       </div>
     </section>
   )
